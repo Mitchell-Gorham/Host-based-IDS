06125283bfb6425cc792520e08f05c8792e0b81c
Seed <seed@ubuntu.(none)>
2019-05-04 00:20:30 -0700

    Clean up and commenting of code+files

--- IDS.sh
+++ IDS.sh
@@ -7,11 +7,11 @@ CPTH="$(pwd)/" #CHECKPATH variable that stores the path to the check file
 
 #Loop through command line arguments checking for -c and -o
 
-dir_loop () {	# CD FIRST then call me over
+dir_loop () {
 	# Recursively loops through each directory and file in the specified directory
 	for i in *
 	do
-		if [ -d $i ]
+		if [ -d $i ]	# Checks if current object is directory, stores data then begins looping though it
 		then
 			echo -n "$(pwd)"/$i >> $VER
 			echo " $(ls -ld $i) " >> $VER
@@ -19,7 +19,7 @@ dir_loop () {	# CD FIRST then call me over
 			dir_loop
 			cd ..
 		else
-		if [ -f $i ]
+		if [ -f $i ]	# Checks if current object is a file, stores all it's data and moves on
 		then
 			echo -n "$(pwd)"/$i  >> $VER
 			echo -n " $(ls -l $i) " >> $VER
@@ -33,7 +33,7 @@ check_loop () {
 	# Recursively loops through each directory and file, checking them against the verification file
 	for i in *
 	do
-		if [ -d $i ]
+		if [ -d $i ]	# Similar
 		then
 			echo -n "$(pwd)"/$i >> $CPTH
 			echo " $(ls -ld $i) " >> $CPTH
@@ -61,7 +61,7 @@ check_files_loop () {
 
 	added=0 	#counter to show how many files have been added
 	deleted=0	#counter to show how many files have been deleted
-	ctr=0
+
 	cat $VER |
 	{
 		while read veri		# Detects Deletions and Modifications
@@ -77,7 +77,7 @@ check_files_loop () {
 
 	cat $CPTH |
 	{
-		while read check
+		while read check	# Detects Additions and Modifications
 		do
 			COUNT=$(grep -c "$check" $VER)
 			if [ $COUNT = 0 ]
@@ -87,10 +87,11 @@ check_files_loop () {
 			fi
 		done
 	}
+	# Save all modified file names
 	MODIFIED="$(sort t.txt | awk '{print $10}' | uniq -iD | uniq -i)"
 	cat "t.txt" |
 	{
-		while read temp
+		while read temp		# Detects deletions and additions
 		do
 			NAMES="$( echo $temp | awk '{print $10}')"
 			COUNT=$(grep -c "$NAMES" "t.txt")
@@ -99,7 +100,7 @@ check_files_loop () {
 				TYPE="$( echo $temp | awk '{print $12}')"
 				if [ "$TYPE" = "-a" ]
 				then
-					ADD="$ADD $NAMES"
+					ADD="$ADD $NAMES" 
 				else
 				if [ "$TYPE" = "-d" ]
 				then
@@ -108,22 +109,25 @@ check_files_loop () {
 				fi
 			fi
 		done
-	 echo "Files created: " $ADD
-	 echo "Files deleted: " $DEL
+
+		# Outputs results to the console
+		echo "Files created: " $ADD
+		echo "Files deleted: " $DEL
 	}
 	echo "Files modified: " $MODIFIED
 }
+##
+##	MAIN
 ##
 for i in "$@"
 do
 	case $i in
 		-c)	# Requires name of file after argument
 			# Create verifcation with the file name given as next argument.
-			echo "Create verification file"
+			echo "Creating verification file called $1"
 			shift
 			# Check if user even entered an argument for -c
 			# Check if entered argument ends in .txt, if it doesn't add it.
-			echo "File Name: $1"	# DEBUG - Display name of file
 			if [ -f $1 ]	# Checks to see if file with same name exists
 			then
 				rm $1	# Removes existing file of the same name if it exists
@@ -136,29 +140,27 @@ do
 		-o)	# Requires verification file and (Optionally)  output file name IN THIS ORDER
 			# Write results to file given as the next argument.
 			shift
-			echo "Checking against verification file: $1" #Do stuff
-			if [ -f "check.txt" ]
+			echo "Checking against verification file: $1"
+			if [ -f "check.txt" ]	# Temp File Creation mangement
 			then
-				rm "check.txt"	#
+				rm "check.txt"
 			fi
-			touch "check.txt"	#
+			touch "check.txt"
 			CPTH="${CPTH}check.txt"
 			VER="$VER$1"
-			check_loop	#
+			check_loop
 			check_files_loop
 			#If output name exists
 			# Move to the output file name
-			if [ "$#" -gt 1 ]
+			if [ "$#" -gt 1 ]	# Checks if user has supplied an output file to save results to
 			then
 				shift
-				echo "Writing results to file: $1"	
+				echo "Writing results to file: $1"
 			fi
-			# If user
-			# echo
 			;;
 
-		-dum)	#
-			#Create dummy folders and files
+		-dum)	# Creates dummy directories and files
+			echo "Creating example directories and files to work with"
 			for i in 1 2 3
 			do
 				if [ ! -d "dir$i" ]
@@ -172,17 +174,21 @@ do
 					echo "$i exists"
 				fi
 			done
-			ln -s file1.txt slink1
-			ls -l file1.txt slink1
+			# Creates a symbolic link
+			#if [ ! -L "slink" ]
+			#then
+			#	ln -s file1.txt slink1
+			#	ls -l file1.txt slink1
+			#fi
 			;;
-		#*)
-		#	echo "You screwed up"
+		#*)	# Potential room for argument catchall (out of scope)
+		#	echo "Catch all"
 		#	;;
 	esac
 	if [ "$#" -gt 1 ] # if num arguments greater than 1
 	then
-#		echo "PreShift $1"
+		#echo "PreShift $1"
 		shift
-#		echo "PostShift $1"
+		#echo "PostShift $1"
 	fi
 done
